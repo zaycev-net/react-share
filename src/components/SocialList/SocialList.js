584e588d481fc81c9b6f69a82595d39b382d61df
DMovsumov <movsumov.denis@gmail.com>
2020-08-22 13:15:00 +0500

    Добавил utm метки + telegram share

--- src/components/SocialList/SocialList.js
+++ src/components/SocialList/SocialList.js
@@ -33,8 +33,9 @@ SocialList.propTypes = {
 	style: PropTypes.objectOf(PropTypes.string),
 	toCount: PropTypes.bool,
 	list: PropTypes.arrayOf(PropTypes.exact({
-		name: PropTypes.oneOf(['vk', 'mail', 'ok', 'facebook', 'twitter']),
-		textButton: PropTypes.string
+		name: PropTypes.oneOf(['vk', 'mail', 'ok', 'facebook', 'twitter', 'telegram']),
+		textButton: PropTypes.string,
+		utm: PropTypes.string
 	}))
 };
 
@@ -43,23 +44,33 @@ SocialList.defaultProps = {
 	list: [
 		{
 			name: 'vk',
-			textButton: 'Вконтакте'
+			textButton: 'Вконтакте',
+			utm: 'vk'
 		},
 		{
 			name: 'mail',
-			textButton: 'Мой мир'
+			textButton: 'Мой мир',
+			utm: 'mail'
 		},
 		{
 			name: 'ok',
-			textButton: 'Одноклассники'
+			textButton: 'Одноклассники',
+			utm: 'ok'
 		},
 		{
 			name: 'facebook',
-			textButton: 'Facebook'
+			textButton: 'Facebook',
+			utm: 'facebook'
 		},
 		{
 			name: 'twitter',
-			textButton: 'Twitter'
+			textButton: 'Twitter',
+			utm: 'twitter'
+		},
+		{
+			name: 'telegram',
+			textButton: 'Telegram',
+			utm: 'telegram'
 		}
 	]
 };
--- src/utils/renderIcon.js
+++ src/utils/renderIcon.js
@@ -5,6 +5,7 @@ import Mail from '../assets/social/mail.svg';
 import OK from '../assets/social/ok.svg';
 import Facebook from '../assets/social/facebook.svg';
 import Twitter from '../assets/social/twitter.svg';
+import Telegram from '../assets/social/telegram.svg';
 
 const renderIcon = name => {
 	switch (name) {
@@ -18,6 +19,8 @@ const renderIcon = name => {
 			return <Facebook/>;
 		case 'twitter':
 			return <Twitter/>;
+		case 'telegram':
+			return <Telegram/>;
 	}
 };
 
